feafba2e630f2670bf44aab199f8bd57614802f5
alfaisaldwi <alfaisaldwi@gmail.com>
2022-02-24 16:19:45 +0700

    Section 4

---
+++ 4_Introduction to Algorithm and Dart Programming/praktikum/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Nomor 1
+
+  double L;
+  int jari = 7;
+  var pi = 22 / 7;
+
+  print(L = pi * (jari * jari));
+
+//Nomor 2
+  String nama = 'Alfaisal Dwiarianto';
+  String jurusan = 'Teknik Informatika';
+  String asal = 'Cirebon';
+
+  print('Nama Saya ' + nama + ' Jurusan ' + jurusan + ' dan asal saya dari Kota ' + asal);
+}
