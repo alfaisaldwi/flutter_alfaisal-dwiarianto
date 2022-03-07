2f80cd72940843b9db2b456dbfb8bd9586de9407
alfaisaldwi <alfaisaldwi@gmail.com>
2022-03-07 18:14:26 +0700

    revisi

--- 7_Advance Function, Async-Await/praktikum/section7_no2.dart
+++ 7_Advance Function, Async-Await/praktikum/section7_no2.dart
@@ -1,18 +1,20 @@
-void main() {
-  var list = [
+void main() async{
+  List list = [
     ['Indonesia', 'Jakarta'],
     ['Vietnam', 'Hanoi'],
     ['Malaysia', 'Kuala Lumpur'],
     ['Kamboja', 'Phnom Penh']
   ];
 
+  print('List Negara $list');
+  print('Map Negara : ');
   for (var i = 0; i < list.length; i++) {
     var negara = [list[i].first];
     var kota = [list[i].last];
-    // print(list[i].first);
-    // print(list[i].last);
 
     Map<String, dynamic> hasil = {'$negara': kota};
-    print(hasil);
+    await Future.delayed(Duration(seconds: 1), (){
+      print(hasil);
+    }); 
   }
 }
